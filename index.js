bba730978313924d6d12f0bd050cb9698999b97e
loren <lorenmichaelh@gmail.com>
2021-12-16 15:53:25 -0600

    commit tree 19152803546da230ff974bd034ec83353afca43e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+
+const BASE_URL = "http://localhost:3000";
+
+
+const newUser = {
+    user: "Loren",
+    email: "loren@loren.com"
+}
+
+
+function submitData(userName, userEmail) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+    return fetch(BASE_URL + `/users`, configObj)
+    .then(resp => resp.json())
+    .then(body => document.getElementsByTagName("body")[0].innerHTML = body.id)
+    .catch(error => document.body.innerHTML = error);
+
+    }
+
+fetch(BASE_URL, submitData);
